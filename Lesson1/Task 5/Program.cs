5c1474d02f42adb22f869f86e3776b0d03ca4b0a
Andrey Lehin <andreylehin@yandex.ru>
2022-08-04 21:12:12 +0300

    Tasks for lesson1

---
+++ Lesson1/Task 5/Program.cs
@@ -0,0 +1,42 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Выводит на экран имя и город  в определенное место экрана
+
+namespace Task_5
+{
+    internal class Program
+    {
+         
+        static void Print (string msq, string town, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Вас зовут "+msq+" ");
+
+            Console.SetCursorPosition(x, y+1);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Вы живете в городе  {town}");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут? ");
+            string msq = Console.ReadLine();
+
+            Console.WriteLine("Где Вы живете? ");
+            string town = Console.ReadLine();
+
+            Console.WriteLine("Введите координаты вывода");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());  
+
+            Print(msq, town, x, y);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//    Анкета
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите Ваше ИМЯ ");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Введите Вашу ФАМИЛИЮ");
+            string Surname = Console.ReadLine();
+
+            Console.WriteLine("Введите Ваш ВОЗРАСТ ");
+            string Age = Console.ReadLine();
+
+            Console.WriteLine("Введите Ваш РОСТ");
+            string Height = Console.ReadLine();
+
+            Console.WriteLine("Введите Ваш ВЕС");
+            string Weight = Console.ReadLine();
+
+            Console.WriteLine(Name+" "+Surname+": возраст "+Age+", рост "+Height+", вес "+Weight);
+            Console.WriteLine("{0} {1}: возраст {2}, рост {3}, вес {4}", Name, Surname, Age, Height, Weight );
+            Console.WriteLine($"{Name} {Surname}: возраст {Age}, рост {Height}, вес {Weight}");
+
+            Console.ReadKey();
+        }
+    }
+}
